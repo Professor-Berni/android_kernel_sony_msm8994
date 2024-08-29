cb2edda6d75e26efa12174283768e8525771233c
Bernhard Thoben <berni-the-bear@web.de>
2024-08-29 16:31:55 +0200

    mm/slub: add missing TID updates on slab deactivation.
    Change-Id: I0ce43d9809fc76bcc2f400215aaca7cc8305bb46

--- mm/slub.c
+++ mm/slub.c
@@ -2333,6 +2333,7 @@ redo:
 		deactivate_slab(s, page, c->freelist);
 		c->page = NULL;
 		c->freelist = NULL;
+		c->tid = next_tid(c->tid);
 		goto new_slab;
 	}
 
@@ -2345,6 +2346,7 @@ redo:
 		deactivate_slab(s, page, c->freelist);
 		c->page = NULL;
 		c->freelist = NULL;
+		c->tid = next_tid(c->tid);
 		goto new_slab;
 	}
 
@@ -2359,6 +2361,7 @@ redo:
 
 	if (!freelist) {
 		c->page = NULL;
+		c->tid = next_tid(c->tid);
 		stat(s, DEACTIVATE_BYPASS);
 		goto new_slab;
 	}
@@ -2384,6 +2387,7 @@ new_slab:
 		c->partial = page->next;
 		stat(s, CPU_PARTIAL_ALLOC);
 		c->freelist = NULL;
+		c->tid = next_tid(c->tid);
 		goto redo;
 	}
 
@@ -2408,6 +2412,7 @@ new_slab:
 	deactivate_slab(s, page, get_freepointer(s, freelist));
 	c->page = NULL;
 	c->freelist = NULL;
+	c->tid = next_tid(c->tid);
 	local_irq_restore(flags);
 	return freelist;
 }
